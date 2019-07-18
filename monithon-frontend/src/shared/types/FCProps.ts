094fec2bb2c0a680216b2b7af308a66b02f0919c
Ali Nosrati <nosrati@inside.sahab.ir>
2019-07-18 14:32:34 +0430

    change directory name

--- monithon-frontend/src/shared/types/FCProps.ts
+++
@@ -1,3 +0,0 @@
-import { ReactNode } from "react";
-
-export type FCProps<Props> = Props extends { children: any } ? Props : Props & { children?: ReactNode };
--- monithon-frontend/src/shared/types/Styles.ts
+++
@@ -1,3 +0,0 @@
-import { ClassKeyOfStyles, ClassNameMap } from '@material-ui/styles/withStyles';
-
-export type Styles<S> = { classes?: Partial<ClassNameMap<ClassKeyOfStyles<S>>> };
